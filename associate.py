b6858e696c3f82ba94ee31b1254db088a979a80d
DESKTOP-QLB6NKR\rlmur <rebeccaLmurray94@gmail.com>
2017-09-30 18:09:59 -0400

    Using Time class inside the Associate Class

--- associate.py
+++ associate.py
@@ -1,12 +1,14 @@
+import timeClasses
+
 class Associate():
     def __init__(self, name, startTime, endTime):
         self.name = name
-        self.startTime = startTime
-        self.endTime = endTime
+        self.startTime = timeClasses.Time( startTime )
+        self.endTime = timeClasses.Time( endTime )
 
     def getName( self ):
         return self.name
     
     def getSchedule( self ):
-        return self.startTime + " - " + self.endTime
+        return self.startTime.getTime() + " - " + self.endTime.getTime()
     
